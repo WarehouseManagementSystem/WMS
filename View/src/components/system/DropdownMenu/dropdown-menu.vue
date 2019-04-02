c7e21a3055d654ce7012703211cbf30d88e4c3d9
MenglinChen <2900810740@qq.com>
2019-04-02 11:52:52 +0800

    edit dropdownMenu show or hide

--- View/src/components/system/DropdownMenu/dropdown-menu.vue
+++ View/src/components/system/DropdownMenu/dropdown-menu.vue
@@ -1,5 +1,5 @@
 <template>
-    <div id="dropdownMenu" style="cursor: default;" class="border-0" @blur="show = false" tabindex="0" contenteditable :model="show">
+    <div id="dropdownMenu" style="cursor: default;" class="border-0" :model="show">
         <a id="dropdownMenuButton" class="btn btn-dark d-inline-flex" style="cursor: default;" href="#" role="button" @click="show = !show">
             <i class="fas fa-bars align-self-center px-1"></i>
             <font class="align-self-center px-1">menu</font>
@@ -23,6 +23,34 @@ export default {
     components: {
         DropdownMenuTooltip,
     },
+    methods: {
+        bindHindeEvents: function () {
+            document.body.addEventListener('mousedown', this.hindeMenu )
+            document.body.addEventListener('mousewheel', this.hindeMenu )
+        },
+        unbindHindeEvents: function () {
+            document.body.removeEventListener('mousedown', this.hindeMenu )
+            document.body.removeEventListener('mousewheel', this.hindeMenu )
+        },
+        hindeMenu: function (event) {
+            const e = event.target
+            // 判断鼠标点击位置是否在菜单内，如果是则不隐藏，如果不是则隐藏
+            this.isChildById(e, 'dropdownMenuTooltip')
+                
+        },
+        isChildById: function (e, id) {
+            if (!(e || e.nodeName) || ['#document', 'HTML', 'BODY'].includes(e.nodeName)) 
+                return this.show = false
+            e = e.parentNode
+            if (e.id && e.id == id) return
+            this.isChildById(e, id)
+        },
+    },
+    watch: {
+        show: function (newValue) {
+            newValue ? this.bindHindeEvents() : this.unbindHindeEvents()
+        }
+    }
 }
 </script>
 
