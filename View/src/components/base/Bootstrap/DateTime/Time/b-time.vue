a77066b95c9d96288233b7d9758138fbb75c70a2
MenglinChen <2900810740@qq.com>
2019-07-09 14:27:51 +0800

    datetime 09 datetime/time/time finished

--- View/src/components/base/Bootstrap/DateTime/Time/b-time.vue
+++ View/src/components/base/Bootstrap/DateTime/Time/b-time.vue
@@ -33,8 +33,20 @@ export default {
             },
         },
         value: String,
-        min: String,
-        max: String,
+        min: {
+            type: String,
+            default: '00:00:00',
+            validator: function (value) {
+                return new Date(...[1970, 1, 1, ...value.split(':')]).toString() !== 'Invalid Date'
+            },
+        },
+        max: {
+            type: String,
+            default: '23:59:59',
+            validator: function (value) {
+                return new Date(...[1970, 1, 1, ...value.split(':')]).toString() !== 'Invalid Date'
+            },
+        },
     },
     computed: {
         now: function () {
@@ -49,10 +61,10 @@ export default {
             }
         },
         dateMin: function () {
-            return new Date('1970-01-01T' + this.min)
+            return this.string2Date(this.min)
         },
         dateMax: function () {
-            return new Date('1970-01-01T' + this.max)
+            return this.string2Date(this.max)
         },
         selectArea: function () {
             let strs = this.time.split(':')
@@ -66,11 +78,9 @@ export default {
         },
     },
     mounted () {
-        let d = new Date('1970-01-01T' + this.value.split(':').map((e) => util.string.padStart(e, 2, '0')).join(':'))
+        let d = this.string2Date(this.value)
         d = d.toString() == 'Invalid Date' ? this.now : d
-        this.date.hh = d.getHours()
-        this.date.mm = d.getMinutes()
-        this.date.ss = d.getSeconds()
+        this.Date2date(this.formatData(d))
         this.showValue()
     },
     methods: {
@@ -84,22 +94,22 @@ export default {
             let s = value.length > 0 ? ':' : ''
             return s + util.string.padStart(call, 2, '0')
         },
-        formatData: function (value, type) {
-            if (type == 'hh') {
-                let dateMinHours = this.dateMin.getHours(), dateMaxHours = this.dateMax.getHours()
-                if (value < dateMinHours) value = dateMinHours
-                if (value > dateMaxHours) value = dateMaxHours
-            } else if (type == 'mm') {
-                let dateMinMinutes = this.dateMin.getMinutes(), dateMaxMinutes = this.dateMin.getMinutes()
-                if (value < dateMinMinutes) value = dateMinMinutes
-                if (value > dateMaxMinutes) value = dateMaxMinutes
-            } else if (type == 'ss') {
-                let dateMinSeconds = this.dateMin.getSeconds(), dateMaxSeconds = this.dateMin.getSeconds()
-                if (value < dateMinSeconds) value = dateMinSeconds
-                if (value > dateMaxSeconds) value = dateMaxSeconds
-            }
-            return value
+        formatData: function (value) {
+            value < this.dateMin 
+                ? this.Date2date(this.dateMin)
+                : value > this.dateMax
+                    ? this.Date2date(this.dateMax)
+                    : this.Date2date(value)
+            
         },
+        string2Date: function (str) {
+            return new Date(...[1970, 1, 1, ...str.split(':')])
+        },
+        Date2date: function (d) {
+            this.date.hh = d.getHours()
+            this.date.mm = d.getMinutes()
+            this.date.ss = d.getSeconds()
+        }, // 将 Date 类型数据转换成 date 变量，以便显示
         setSelectArea: function (element, start = 0) {
             if (start < 0) return
             if (start <= this.selectArea.one.end) {
@@ -130,35 +140,22 @@ export default {
                 let { value, selectionStart, selectionEnd } = vm.$el.children[0]
                 if (selectionStart !== selectionEnd) return
 
-                let array = value.split(':').map((e) => util.string.padStart(e, 2, '0'))
-                let d = array.join(':')
+                let d = this.string2Date(value)
                 let start = selectionStart // 记录下一个选择区域的起始点
 
-                if (new Date('1970-01-01T' + d).toString() !== 'Invalid Date') { 
-                    let type, str
+                if (d.toString() !== 'Invalid Date') { 
                     if (selectionStart <= vm.selectArea.one.end) {
-                        if (vm.types.hh) type = 'hh'
-                        else if (vm.types.mm) type = 'mm'
-                        else type = 'ss'
-                        str = d.substring(vm.selectArea.one.start, vm.selectArea.one.end)
-                        vm.date.hh = vm.formatData(str, type)
                         start = vm.selectArea.two.start
                     } else if (selectionStart >= vm.selectArea.two.start && selectionStart <= vm.selectArea.two.end) {
-                        if (vm.types.hh) type = 'mm'
-                        else type = 'ss'
-                        str = d.substring(vm.selectArea.two.start, vm.selectArea.two.end)
-                        vm.date.mm = vm.formatData(str, type)
                         start = vm.selectArea.three.start
                     } else if (selectionStart > vm.selectArea.three.start) {
-                        type = 'ss'
-                        str = d.substring(vm.selectArea.three.start)
-                        vm.date.ss = vm.formatData(str, type)
                         start = -1 // 表示不再选择
                     }
+                    vm.formatData(d)
                 } else {
                     // 输入非法时修正 start，以正确创建选择区域
                     // 当分段字符数超过 2 时会导致选择区域错误
-                    let str = array.find(function(element) {
+                    let str = value.split(':').map((e) => util.string.padStart(e, 2, '0')).find(function(element) {
                         return element.toString().length > 2;
                     });
                     if (str) start = start - (str.toString().length - 2)
