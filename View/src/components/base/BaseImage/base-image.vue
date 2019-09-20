ca10d9408fe7f475bd1997d658733eeb70899974
MenglinChen <2900810740@qq.com>
2019-09-20 13:15:04 +0800

    [b-media] has bug hide [b-media-demo] in menu

--- View/src/components/base/BaseImage/base-image.vue
+++ View/src/components/base/BaseImage/base-image.vue
@@ -1,9 +1,10 @@
 <template>
 ​   <picture>
-        <source v-for="(img, index) in img-list" :key="index" :srcset="img.src" :media="img.media" :type="img.type">
+        <source v-for="(img, index) in list" :key="index" :srcset="img.src" :media="img.media" :type="img.type">
         <img :src="imgSrc" class="img-fluid img-thumbnail" :alt="alt">
     </picture>
 </template>
+
 <script>
 export default {
     name: 'base-image',
@@ -14,15 +15,15 @@ export default {
         alt: {
             type: String,
         },
-        imgList: {
+        list: {
             type: Array,
         },
     },
     computed: {
         imgSrc: function () {
             if (this.src) return this.src
-            const src = this.imgList[this.imgList.length].src
-            return src ? src : ''
+            const src = this.list[this.list.length - 1].src
+            return src || ''
         },
     },
 }
