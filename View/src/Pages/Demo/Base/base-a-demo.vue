f4460a448cfe87a2f8eaa6d639d7f0abdc328cec
MenglinChen <chanmenglin@icloud.com>
2019-09-24 10:40:54 +0800

    edit [base-a]

--- View/src/Pages/Demo/Base/base-a-demo.vue
+++ View/src/Pages/Demo/Base/base-a-demo.vue
@@ -9,6 +9,7 @@
         4. 链接
     href 属性为必填，如若发现没有可用的 href 值则很可能出现了 base-a 的滥用，请考虑使用其它控件/标签，
     除定位到顶部，其它用途请不要使用 ‘#’、‘#top’ 作为 href 的值，这仍然是滥用行为，请严格按照以上用途使用
+    （本项目中的滥用是出于样式展示目的，实际使用时请给予有意义的 href 值）
         </pre>
         <hr>
         <div class="row py-1">
@@ -24,15 +25,16 @@
             <div class="col-11">
                 <div class="row">
                     <font class="col-2">tel: </font>
-                    <base-a href="tel:15872955525"></base-a>
+                    <base-a href="tel:电话号码"></base-a>
                 </div>
                 <div class="row">
                     <font class="col-2">sms: </font>
-                    <base-a href="sms:15872955525"></base-a>
+                    <base-a href="sms:电话号码"></base-a>
                 </div>
                 <div class="row">
                     <font class="col-2">mailto: </font>
-                    <base-a href="mailto:2900910740@qq.com"></base-a>
+                    <base-a href="mailto:邮件地址"></base-a>（详情：<base-a href="https://developer.mozilla.org/zh-CN/docs/Web/Guide/HTML/Email_links">Email links | MDN
+</base-a>）
                 </div>
                 <div class="row">
                     <font class="col-2">link: </font>
@@ -41,7 +43,7 @@
                 </div>
                 <div class="row">
                     <font class="col-2">Anchor point: </font>
-                    <base-a class="px-1" ></base-a>
+                    <base-a class="px-1" href="#" ></base-a>
                     <base-a class="px-1" href="#top"></base-a>
                     <base-a class="px-1" href="#1-1"></base-a>
                 </div>
@@ -57,7 +59,7 @@
         </div>
         <div class="row py-1">
             <font class="col-1">icon</font>
-            <base-a text="text" icon="fas fa-info"></base-a>
+            <base-a href="#" text="text" icon="fas fa-info"></base-a>
         </div>
     </div>
 </template>
