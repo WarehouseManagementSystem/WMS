45525e95c7b67f4716a63fe0146976800676ec88
MenglinChen <chanmenglin@icloud.com>
2020-03-16 11:52:45 +0800

    edit error for deploy

--- View/src/components/base/Content/Video/c-video.vue
+++ View/src/components/base/Content/Video/c-video.vue
@@ -75,7 +75,7 @@ export default {
             ],
         }
         this.player = videojs(this.$refs.videoPlayer, this.options, function onPlayerReady() {
-            console.log('onPlayerReady', this);
+            
         })
         
     },
